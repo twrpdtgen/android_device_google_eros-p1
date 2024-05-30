e436651f8e48bc77059b000ecce378632e0ef91e
Sebastiano Barezzi <barezzisebastiano@gmail.com>
2024-05-30 19:56:33 +0200

    eros-p1: Initial TWRP device tree Made with SebaUbuntu's TWRP device tree generator Arch: arm Manufacturer: google Model: GameBox Script version: 3.0.0

---
+++ AndroidProducts.mk
@@ -0,0 +1,14 @@
+#
+# Copyright (C) 2024 The Android Open Source Project
+# Copyright (C) 2024 SebaUbuntu's TWRP device tree generator
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+PRODUCT_MAKEFILES := \
+    $(LOCAL_DIR)/omni_eros-p1.mk
+
+COMMON_LUNCH_CHOICES := \
+    omni_eros-p1-user \
+    omni_eros-p1-userdebug \
+    omni_eros-p1-eng
